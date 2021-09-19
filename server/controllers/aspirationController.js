bc3b90930c57460e567fc17aaf7e4ee232d94250
asraavisena <asra.avisena@yahoo.com>
2021-09-19 12:06:54 +0700

    create and read aspiration done

--- server/controllers/aspirationController.js
+++ server/controllers/aspirationController.js
@@ -1,16 +1,65 @@
 const Aspiration = require("../models/aspiration");
+const Dinas = require("../models/dinas");
+const User = require("../models/user");
 
 class AspirationController {
   static async getAll(req, res, next) {
+    const { id } = req.user;
+
     try {
-      res.send("test");
+      const getAspirations = await Aspiration.find({ user: id });
+      res.status(200).json(getAspirations);
     } catch (err) {
-      console.log(err);
+      next(err);
+    }
+  }
+
+  static async getById(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const getAspiration = await Aspiration.findOne({ _id: id });
+      res.status(200).json(getAspiration);
+    } catch (err) {
+      next(err);
     }
   }
 
   static async create(req, res, next) {
+    const { id } = req.user;
+
+    const payload = {
+      title: req.body.title,
+      description: req.body.description,
+      type: req.body.type,
+      user: id,
+    };
     try {
+      const dinas = await Dinas.findOne({ _id: req.body.dinas });
+      payload.dinas = dinas;
+      const createAspiration = await Aspiration.create(payload);
+      await Dinas.findByIdAndUpdate(
+        { _id: req.body.dinas },
+        {
+          $push: {
+            aspirations: { _id: createAspiration._id, ...payload },
+          },
+        },
+        { new: true, useFindAndModify: false }
+      );
+      await User.findByIdAndUpdate(
+        { _id: id },
+        {
+          $push: {
+            aspirations: { _id: createAspiration._id, ...payload },
+          },
+        },
+        { new: true, useFindAndModify: false }
+      );
+      const foundAspiration = await Aspiration.findOne({
+        _id: createAspiration._id,
+      });
+      res.status(201).json(foundAspiration);
     } catch (err) {
       console.log(err);
     }
--- server/routers/aspiration.js
+++ server/routers/aspiration.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const AspirationController = require("../controllers/aspirationController");
+const { Userauth } = require("../middlewares/auth");
+
+router.use(Userauth);
 
 router.get("/", AspirationController.getAll);
 
-router.post("/create", AspirationController.create);
+router.get("/:id", AspirationController.getById);
+
+router.post("/", AspirationController.create);
 
 module.exports = router;
