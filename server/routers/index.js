2b34095389658435c112a258f354da4ea4ba176a
asraavisena <asra.avisena@yahoo.com>
2021-09-19 15:23:08 +0700

    notification done

--- server/routers/index.js
+++ server/routers/index.js
@@ -3,10 +3,12 @@ const userRouters = require("./user");
 const reportUserRouters = require("./reportUser");
 const dinasRouters = require("./dinas/dinas");
 const aspirationRouters = require("./aspiration");
+const notificationsRouters = require("./notifications");
 
 routers.use("/", userRouters);
 routers.use("/reportUser", reportUserRouters);
 routers.use("/dinas", dinasRouters);
 routers.use("/aspirations", aspirationRouters);
+routers.use("/notifications", notificationsRouters);
 
 module.exports = routers;
