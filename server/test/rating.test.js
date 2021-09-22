269a74e9fb0efddc487c7fc14963b18e76eb2810
asraavisena <asra.avisena@yahoo.com>
2021-09-22 11:21:09 +0700

    finished rating testing still on mock up not yet

--- server/test/rating.test.js
+++ server/test/rating.test.js
@@ -6,79 +6,90 @@ const User = require("../models/user");
 const Dinas = require("../models/dinas");
 const { jwtSign } = require("../helpers/jwt");
 const Report = require("../models/report");
+const Categories = require("../models/categories");
 
-let user;
-let user2;
-let user3;
-let dinas;
-let ratings;
-let report;
-let report2;
-let report3;
+const dinasDinas = [
+  {
+    name: "Dinas perhubungan",
+    email: "perhubungan@test.com",
+    password: "123456",
+    role: "dinas",
+  },
+  {
+    name: "Dinas Pengamanan",
+    email: "dinasPengamananeport@test.com",
+    password: "test1234",
+    role: "dinas",
+  },
+];
 
-beforeAll((done) => {
-  const dummyUser = {
+const users = [
+  {
     NIK: "1501111709990010",
     fullname: "test",
-    email: "oke@gmail.com",
+    email: "oke123455@test.com",
     password: "123456",
     kota: "jakarta",
     isActive: true,
     ktp: "google.com",
-  };
-  const dummyUser2 = {
+  },
+  {
     NIK: "1501111709990013",
     fullname: "test",
-    email: "okes@gmail.com",
+    email: "oke123455s@test.com",
     password: "123456",
     kota: "jakarta",
     isActive: true,
     ktp: "google.com",
-  };
-  const dummyUser3 = {
+  },
+  {
     NIK: "1501111709990012",
     fullname: "test",
-    email: "okeaja@gmail.com",
+    email: "oke123455aja@test.com",
     password: "123456",
     kota: "jakarta",
     isActive: true,
     ktp: "google.com",
-  };
-  const dummyUser4 = {
-    NIK: "1501111709990052",
-    fullname: "test",
-    email: "okeajan@gmail.com",
-    password: "123456",
-    kota: "jakarta",
-    isActive: true,
-    ktp: "google.com",
-  };
-  const dummyUser5 = {
-    NIK: "1501111709990032",
-    fullname: "test",
-    email: "sip@gmail.com",
-    password: "123456",
-    kota: "jakarta",
-    isActive: true,
-    ktp: "google.com",
-  };
-  const dummyUser6 = {
-    NIK: "1501111709990022",
-    fullname: "test",
-    email: "okesip@gmail.com",
-    password: "123456",
-    kota: "jakarta",
-    isActive: true,
-    ktp: "google.com",
-  };
-  const dummyDinas = {
-    name: "Dinas perhubungan",
-    email: "perhubungan@gmail.com",
-    password: "123456",
-  };
-  const dummyReport = {
+  },
+];
+
+const reports = [
+  {
     title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
+    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
+    location: "req.body.location",
+    lat: -6.219555543570665,
+    long: 106.81447449443793,
+    picture:
+      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
+    issuedDate: new Date(),
+    status: "diterima",
+  },
+  {
+    title: "banjir nih bos",
+    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
+    location: "req.body.location",
+    lat: -6.219555543570665,
+    long: 106.81447449443793,
+    picture:
+      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
+    issuedDate: new Date(),
+    status: "diterima",
+  },
+  {
+    title: "banjir nih bos",
+    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
+    location: "req.body.location",
+    lat: -6.219555543570665,
+    long: 106.81447449443793,
+    picture:
+      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
+    issuedDate: new Date(),
+    status: "diterima",
+  },
+  {
+    title: "banjir nih bos",
+
     description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
     location: "req.body.location",
     lat: -6.219555543570665,
@@ -86,131 +97,88 @@ beforeAll((done) => {
     category: "6145f9e8ecc3a5b61970be20",
     picture:
       "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyReport2 = {
-    title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
-    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
-    location: "req.body.location",
-    lat: -6.219555543570665,
-    long: 106.81447449443793,
-    category: "6145f9e8ecc3a5b61970be20",
-    picture:
-      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyReport3 = {
-    title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
-    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
-    location: "req.body.location",
-    lat: -6.219555543570665,
-    long: 106.81447449443793,
-    category: "6145f9e8ecc3a5b61970be20",
-    picture:
-      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyReport4 = {
-    title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
-    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
-    location: "req.body.location",
-    lat: -6.219555543570665,
-    long: 106.81447449443793,
-    category: "6145f9e8ecc3a5b61970be20",
-    picture:
-      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyReport5 = {
-    title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
-    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
-    location: "req.body.location",
-    lat: -6.219555543570665,
-    long: 106.81447449443793,
-    category: "6145f9e8ecc3a5b61970be20",
-    picture:
-      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyReport6 = {
-    title: "banjir nih bos",
-    dinas: "6148b1079efa60bb9eefce4c",
-    description: "req.body.description sdkjaskdjaksjdklas kasldklasjdk",
-    location: "req.body.location",
-    lat: -6.219555543570665,
-    long: 106.81447449443793,
-    category: "6145f9e8ecc3a5b61970be20",
-    picture:
-      "https://statik.tempo.co/data/2021/09/17/id_1051653/1051653_720.jpg",
-  };
-  const dummyRating = {
+    issuedDate: new Date(),
+    status: "diterima",
+  },
+];
+
+const categories = [
+  {
+    name: "kecelakaan",
+  },
+  {
+    name: "kriminal",
+  },
+];
+
+const ratings = [
+  {
     rating: 5,
     comment: "mantap abis nih bos",
-  };
-  User.create(dummyUser)
-    .then((data) => {
-      user = data;
-      dummyRating.user = data._id;
-      dummyReport.user = data._id;
-      return User.create(dummyUser2);
+  },
+];
+
+let user = {};
+let user1 = {};
+let user2 = {};
+let dinas = {};
+let dinas1 = {};
+let rating = {};
+let rating1 = {};
+let report = {};
+let report1 = {};
+let report2 = {};
+let report3 = {};
+let category = {};
+let category1 = {};
+
+beforeAll((done) => {
+  Dinas.create(dinasDinas)
+    .then((res) => {
+      dinas = res[0];
+      dinas1 = res[1];
+      return Categories.create(categories);
     })
-    .then((data) => {
-      user2 = data;
-      dummyReport2.user = data._id;
-      return User.create(dummyUser3);
+    .then((res) => {
+      category = res[0];
+      category1 = res[0];
+      return User.create(users);
     })
-    .then((data) => {
-      user3 = data;
-      dummyReport3.user = data._id;
-      return User.create(dummyUser4);
+    .then((res) => {
+      user = res[0];
+      user1 = res[1];
+      user2 = res[2];
+      reports[0].user = user;
+      reports[0].dinas = dinas;
+      reports[0].category = category;
+
+      reports[1].user = user1;
+      reports[1].dinas = dinas;
+      reports[1].category = category1;
+
+      reports[2].user = user;
+      reports[2].dinas = dinas1;
+      reports[2].category = category;
+
+      reports[3].user = user;
+      reports[3].dinas = dinas1;
+      reports[3].category = category;
+
+      return Report.create(reports);
     })
-    .then((data) => {
-      user4 = data;
-      dummyReport4.user = data._id;
-      return User.create(dummyUser5);
+    .then((res) => {
+      report = res[0];
+      report1 = res[1];
+      report2 = res[2];
+      report3 = res[3];
+
+      ratings[0].user = user._id;
+      ratings[0].report = report._id;
+      ratings[0].dinas = dinas._id;
+      return Rating.create(ratings);
     })
-    .then((data) => {
-      user5 = data;
-      dummyReport5.user = data._id;
-      return User.create(dummyUser6);
-    })
-    .then((data) => {
-      user6 = data;
-      dummyReport6.user = data._id;
-      return Dinas.create(dummyDinas);
-    })
-    .then((data) => {
-      dinas = data;
-      dummyRating.dinas = data._id;
-      return Report.create(dummyReport);
-    })
-    .then((data) => {
-      report = data;
-      dummyRating.report = data._id;
-      return Report.create(dummyReport2);
-    })
-    .then((data) => {
-      report2 = data;
-      dummyReport.report = data._id;
-      return Report.create(dummyReport3);
-    })
-    .then((data) => {
-      report3 = data;
-      return Report.create(dummyReport4);
-    })
-    .then((data) => {
-      report4 = data;
-      return Report.create(dummyReport5);
-    })
-    .then((data) => {
-      report5 = data;
-      return Report.create(dummyReport6);
-    })
-    .then((data) => {
-      report6 = data;
-      return Rating.create(dummyRating);
-    })
-    .then((data) => {
-      ratings = data;
+    .then((res) => {
+      rating = res[0];
       done();
     })
     .catch((err) => {
@@ -221,13 +189,16 @@ beforeAll((done) => {
 afterAll((done) => {
   User.deleteMany()
     .then((_) => {
-      return Rating.deleteMany();
+      return Dinas.deleteMany();
+    })
+    .then((_) => {
+      return Categories.deleteMany();
     })
     .then((_) => {
       return Report.deleteMany();
     })
-    .then((_) => {
-      return Dinas.deleteMany();
+    .then(() => {
+      return Rating.deleteMany();
     })
     .then((_) => {
       mongoose.connection.close();
@@ -239,12 +210,12 @@ afterAll((done) => {
 });
 
 describe("GET /rating [SUCCESS CASE]", () => {
-  jest.setTimeout(30000);
   test("should return an object with key: id,rating,comment,report,user,dinas", (done) => {
     let access_token = jwtSign({
       id: user._id,
       email: user.email,
     });
+
     request(app)
       .get("/rating")
       .set("Accept", "application/json")
@@ -265,6 +236,7 @@ describe("GET /rating [SUCCESS CASE]", () => {
         );
         done();
       })
+
       .catch((err) => {
         done(err);
       });
@@ -313,6 +285,7 @@ describe("GET /dinas/rating [SUCCESS CASE]", () => {
     let access_token = jwtSign({
       id: dinas._id,
       email: dinas.email,
+      role: "dinas",
     });
     request(app)
       .get("/dinas/rating")
@@ -345,8 +318,9 @@ describe("GET /dinas/rating/:id [SUCCESS CASE]", () => {
     let access_token = jwtSign({
       id: dinas._id,
       email: dinas.email,
+      role: "dinas",
     });
-    let id = ratings._id;
+    let id = rating._id;
     request(app)
       .get(`/dinas/rating/${id}`)
       .set("Accept", "application/json")
@@ -363,6 +337,36 @@ describe("GET /dinas/rating/:id [SUCCESS CASE]", () => {
             dinas: expect.any(String),
           })
         );
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
+
+describe("GET /dinas/rating/:id [SUCCESS FAILED]", () => {
+  const falseID = "6139bfa08c2956b92b58329a";
+
+  test("should ERROR because of [NO ID FOUND] and status 404", (done) => {
+    let access_token = jwtSign({
+      id: dinas._id,
+      email: dinas.email,
+      role: "dinas",
+    });
+
+    request(app)
+      .get(`/dinas/rating/${falseID}`)
+      .set("Accept", "application/json")
+      .set("access_token", access_token)
+      .then((response) => {
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            message: `Rating with id ${falseID} not found`,
+          })
+        );
+
         done();
       })
       .catch((err) => {
@@ -472,8 +476,8 @@ describe("POST /rating [ERROR CASE]", () => {
   });
   test("Expect Error when property rating is null", (done) => {
     let access_token = jwtSign({
-      id: user3._id,
-      email: user3.email,
+      id: user2._id,
+      email: user2.email,
     });
     let ratingNull = {
       rating: null,
@@ -497,8 +501,8 @@ describe("POST /rating [ERROR CASE]", () => {
   });
   test("Expect Error when property rating is string", (done) => {
     let access_token = jwtSign({
-      id: user3._id,
-      email: user3.email,
+      id: user2._id,
+      email: user2.email,
     });
     let ratingNull = {
       rating: "null",
@@ -524,14 +528,14 @@ describe("POST /rating [ERROR CASE]", () => {
   });
   test("Expect Error when property comment is null", (done) => {
     let access_token = jwtSign({
-      id: user4._id,
-      email: user4.email,
+      id: user._id,
+      email: user.email,
     });
     let commentNull = {
       rating: 5,
       comment: null,
       dinas: dinas._id,
-      report: report4._id,
+      report: report3._id,
     };
     request(app)
       .post(`/rating`)
@@ -541,60 +545,6 @@ describe("POST /rating [ERROR CASE]", () => {
       .then((response) => {
         expect(response.status).toBe(400);
         expect(response.body.message).toContain("Comment is required");
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
-  });
-  test("Expect Error when property dinas is wrong", (done) => {
-    let access_token = jwtSign({
-      id: user6._id,
-      email: user6.email,
-    });
-    let dinasNull = {
-      rating: 5,
-      comment: "mantap nih",
-      dinas: "123",
-      report: report6._id,
-    };
-    request(app)
-      .post(`/rating`)
-      .send(dinasNull)
-      .set("Accept", "application/json")
-      .set("access_token", access_token)
-      .then((response) => {
-        expect(response.status).toBe(400);
-        expect(response.body.message).toContain(
-          "This field should be included"
-        );
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
-  });
-  test("Expect Error when property report is wrong", (done) => {
-    let access_token = jwtSign({
-      id: user._id,
-      email: user.email,
-    });
-    let reportWrong = {
-      rating: 5,
-      comment: "mantap nih",
-      dinas: dinas._id,
-      report: "123",
-    };
-    request(app)
-      .post(`/rating`)
-      .send(reportWrong)
-      .set("Accept", "application/json")
-      .set("access_token", access_token)
-      .then((response) => {
-        expect(response.status).toBe(400);
-        expect(response.body.message).toContain(
-          "This field should be included"
-        );
         done();
       })
       .catch((err) => {
